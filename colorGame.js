1819793b633a5314505ed39127b29eaeb60fe83f
Vasanth Anuraag Neppalli <neppallv@canisius.edu>
2017-07-04 14:58:48 -0400

    made it change to the same color

--- colorGame.js
+++ colorGame.js
@@ -1,4 +1,4 @@
-var colors = [ 
+var colors = [
 	"rgb(255, 0, 0)",
 	"rgb(255, 255, 0)",
 	"rgb(0, 255, 0)",
@@ -11,22 +11,34 @@ var colors = [
 var squares = document.querySelectorAll(".square");
 var pickedColor = colors[3];
 var colorDisplay = document.getElementById("color-display");
+var messageDisplay = document.querySelector("#message");
 
 colorDisplay.textContent = pickedColor;
 
-for(var i = 0; i < squares.length; i++) {
+for (var i = 0; i < squares.length; i++) {
     //add initial colors to squares
     squares[i].style.backgroundColor = colors[i];
-    
+
     //add click listeners to squares
-    squares[i].addEventListener("click", function(){
+    squares[i].addEventListener("click", function () {
         //Grab color of clicked square
         var clickedColor = this.style.backgroundColor;
         //compare color to picked Color
-        if(clickedColor === pickedColor) {
-            alert("Correct!");
+        if (clickedColor === pickedColor) {
+            messageDisplay.textContent = "correct";
+            changeColors(clickedColor);
         } else {
-            alert("wrong!");
+            this.style.backgroundColor = "#232323";
+            messageDisplay.textContent = "Try Again";
         }
     });
+}
+
+/**
+Loop through all squares
+*/
+function changeColors(color) {
+    for(var i = 0; i < squares.length; i++) {
+        squares[i].style.backgroundColor = color;
+    }
 }
